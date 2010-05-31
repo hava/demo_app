6e1d690f7c06a13654de993416322cefa12506e6
hava sayed <hava@bright-sun.org>
2010-05-31 11:13:06 +0100

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '369fa9f26c7470053b3879c262f7341c269260a86b7a40fdcb6aef14b782ee5f412a506dd76bf8cc97c64d3257e4fe216d1d4531e5eb051393c4041eedfe20ec';
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_demo_app_session',
+  :secret      => '3d1f156412bf174da216f7e477560a5d4d28da10a646bb992411147c871a47f3cefff16ed1c39ef825e5482309adba6247a582fd6d25cb54c81749c26a4101c7'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
